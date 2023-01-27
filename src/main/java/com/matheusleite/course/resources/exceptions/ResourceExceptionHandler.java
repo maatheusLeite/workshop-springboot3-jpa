81385f6f5d46a97dc725721571b8cf4db00b0165
Matheus Leite <maatheus2710@gmail.com>
2023-01-27 06:06:27 -0300

    Exception handling - delete

--- src/main/java/com/matheusleite/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/matheusleite/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.matheusleite.course.services.exceptions.DatabaseException;
 import com.matheusleite.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String errorDescription = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError(Instant.now(), status.value(), errorDescription, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String errorDescription = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError error = new StandardError(Instant.now(), status.value(), errorDescription, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(error);
 	}
--- src/main/java/com/matheusleite/course/services/UserService.java
+++ src/main/java/com/matheusleite/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.matheusleite.course.entities.User;
 import com.matheusleite.course.repositories.UserRepository;
+import com.matheusleite.course.services.exceptions.DatabaseException;
 import com.matheusleite.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {			
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
